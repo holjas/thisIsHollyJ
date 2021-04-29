c03498b9200cc0a609e9d50e83ffbf0c49e6d75a
holjas <holly.jasiura@gmail.com>
2021-04-28 21:05:47 -0400

    little touches

--- js/script.js
+++ js/script.js
@@ -1,12 +1,63 @@
 const portfolio = {};
 
 portfolio.fadeInScroll = () => {
-  console.log("fadeinscroll link working");
-  //created functions go here.
+  $(window).on("load", function () {
+    $(window)
+      .scroll(function () {
+        let windowBottom = $(this).scrollTop() + $(this).innerHeight();
+
+        $(".fade").each(function () {
+          /* Check the location of each desired element */
+          let objectBottom = $(this).offset().top + $(this).outerHeight();
+          let objectTest = objectBottom - 400;
+          /* If the element is completely within bounds of the window, fade it in */
+          if (objectTest < windowBottom) {
+            //object comes into view (scrolling down)
+            if ($(this).css("opacity") == 0) {
+              $(this).fadeTo(900, 1);
+            }
+          } else {
+            //object goes out of view (scrolling up)
+            if ($(this).css("opacity") == 1) {
+              $(this).fadeTo(900, 0);
+            }
+          }
+        });
+      })
+      .scroll();
+  });
+};
+portfolio.fadeInScrollLarge = () => {
+  $(window).on("load", function () {
+    $(window)
+      .scroll(function () {
+        let windowBottom = $(this).scrollTop() + $(this).innerHeight();
+
+        $(".fadeLarge").each(function () {
+          /* Check the location of each desired element */
+          let objectBottom = $(this).offset().top + $(this).outerHeight();
+          let objectTest = objectBottom - 1900;
+          /* If the element is completely within bounds of the window, fade it in */
+          if (objectTest < windowBottom) {
+            //object comes into view (scrolling down)
+            if ($(this).css("opacity") == 0) {
+              $(this).fadeTo(900, 1);
+            }
+          } else {
+            //object goes out of view (scrolling up)
+            if ($(this).css("opacity") == 1) {
+              $(this).fadeTo(900, 0);
+            }
+          }
+        });
+      })
+      .scroll();
+  });
 };
 
 portfolio.init = () => {
   portfolio.fadeInScroll();
+  portfolio.fadeInScrollLarge();
 };
 
 portfolio.init();
