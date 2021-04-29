4ee5ac962a5d1c5ca5674b09512d227519202e27
holjas <holly.jasiura@gmail.com>
2021-04-29 16:53:20 -0400

    why did it break that last time?

--- js/script.js
+++ js/script.js
@@ -2,12 +2,22 @@ const portfolio = {};
 
 portfolio.scollingFade = () => {
   $(document).ready(function () {
-    $(".fadeOne").scrollFadeOut(1200);
-    $(".fadeTwo").scrollFadeIn(1000);
-
-    $(".fadeThree").scrollFadeIn(1800);
-    $(".fadeFour").scrollFadeIn(2700);
-    $(".fadeFive").scrollFadeIn(5000);
+    //fade on scroll only when scroll is wider than 1000px
+    if ($(window).height() > 900) {
+      $(".fadeOne").scrollFadeOut();
+      $(".fadeTwo").scrollFadeIn(1000);
+      $(".fadeThree").scrollFadeIn(1800);
+      $(".fadeFour").scrollFadeIn(2700);
+      $(".fadeFive").scrollFadeIn(5000);
+    } else if ($(window).height() > 700) {
+      $(".fadeOne").scrollFadeOut();
+      $(".fadeTwo").scrollFadeIn(800);
+      $(".fadeThree").scrollFadeIn(1500);
+      $(".fadeFour").scrollFadeIn(2400);
+      $(".fadeFive").scrollFadeIn(4300);
+    } else if ($(window).height() < 700) {
+      $(".fadeOne").scrollFadeOut();
+    }
   });
 };
 
