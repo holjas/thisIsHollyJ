5ecdbeb803827e640ce0e477fb73ba7f24141800
holjas <holly.jasiura@gmail.com>
2021-04-29 22:06:35 -0400

    three

--- js/script.js
+++ js/script.js
@@ -2,17 +2,18 @@ const portfolio = {};
 
 portfolio.scollingFade = () => {
   $(document).ready(function () {
-    const sectionOne = $(".fadeOne")[0].offsetTop;
     const sectionTwo = $(".fadeTwo")[0].offsetTop;
     const sectionThree = $(".fadeThree")[0].offsetTop;
     const sectionFour = $(".fadeFour")[0].offsetTop;
     const sectionFive = $(".fadeFive")[0].offsetTop;
 
-    $(".fadeOne").scrollFadeOut();
-    $(".fadeTwo").scrollFadeIn(sectionTwo);
-    $(".fadeThree").scrollFadeIn(sectionThree);
-    $(".fadeFour").scrollFadeIn(sectionFour);
-    $(".fadeFive").scrollFadeIn(sectionFive);
+    if ($(window).height() < 700) {
+      $(".fadeOne").scrollFadeOut();
+      $(".fadeTwo").scrollFadeIn(sectionTwo);
+      $(".fadeThree").scrollFadeIn(sectionThree);
+      $(".fadeFour").scrollFadeIn(sectionFour);
+      $(".fadeFive").scrollFadeIn(sectionFive);
+    }
   });
 };
 
