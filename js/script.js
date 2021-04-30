9d4848436480f9560228f9ac7f3a393b6d059c97
holjas <holly.jasiura@gmail.com>
2021-04-29 22:01:49 -0400

    two

--- js/script.js
+++ js/script.js
@@ -2,25 +2,17 @@ const portfolio = {};
 
 portfolio.scollingFade = () => {
   $(document).ready(function () {
-    const windowHeight = $(window).height();
-    const large = 900;
-    const medium = 700;
-    //fade on scroll only when scroll is wider than 1000px
-    if (windowHeight > large) {
-      $(".fadeOne").scrollFadeOut();
-      $(".fadeTwo").scrollFadeIn(1000);
-      $(".fadeThree").scrollFadeIn(1800);
-      $(".fadeFour").scrollFadeIn(2500);
-      $(".fadeFive").scrollFadeIn(4600);
-    } else if (windowHeight < large && windowHeight > medium) {
-      $(".fadeOne").scrollFadeOut();
-      $(".fadeTwo").scrollFadeIn(800);
-      $(".fadeThree").scrollFadeIn(1600);
-      $(".fadeFour").scrollFadeIn(2400);
-      $(".fadeFive").scrollFadeIn(4300);
-    } else {
-      $(".fadeOne").scrollFadeOut();
-    }
+    const sectionOne = $(".fadeOne")[0].offsetTop;
+    const sectionTwo = $(".fadeTwo")[0].offsetTop;
+    const sectionThree = $(".fadeThree")[0].offsetTop;
+    const sectionFour = $(".fadeFour")[0].offsetTop;
+    const sectionFive = $(".fadeFive")[0].offsetTop;
+
+    $(".fadeOne").scrollFadeOut();
+    $(".fadeTwo").scrollFadeIn(sectionTwo);
+    $(".fadeThree").scrollFadeIn(sectionThree);
+    $(".fadeFour").scrollFadeIn(sectionFour);
+    $(".fadeFive").scrollFadeIn(sectionFive);
   });
 };
 
