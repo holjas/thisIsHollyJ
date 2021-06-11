f42e7e3188711c8747834f5ada5edb2237e07cdb
holjas <holly.jasiura@gmail.com>
2021-06-11 12:43:38 -0400

    :art:

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PortfolioNew
+
+Built using 
+Bootstrap
+SASS
---
+++ js/colours.js
@@ -0,0 +1,36 @@
+const colourThemes = [
+    { colorOne: "30, 59, 52", colorTwo: "252, 108, 17" },
+    { colorOne: "255, 209, 0", colorTwo: "222, 16, 222" },
+    { colorOne: "245, 204, 207", colorTwo: "1, 178, 139" },
+    { colorOne: "252, 59, 45", colorTwo: "14, 47, 86" },
+    { colorOne: "17,35,120", colorTwo: "246, 74, 0" },
+    { colorOne: "40,41,43", colorTwo: "255,0,70" },
+    { colorOne: "229, 232, 220", colorTwo: "246, 116, 141" },
+
+];
+
+
+
+// Green 30, 59, 52
+// Orange 252, 108, 17
+
+// Yellow 255, 209, 0
+// Pink/purple 222, 16, 222
+
+// Pink 245, 204, 207
+// Green 1, 178, 139
+
+// Orange 252, 59, 45
+// Navy 14, 47, 86
+
+// Blue 17,35,120
+// Orange 246, 74, 0
+
+// Orange 252, 108, 17
+// Dk green 30, 59, 52
+
+// blk 40,41,43
+// Red 255,0,70
+
+// Grey 229, 232, 220
+// Pink 246, 116, 141
