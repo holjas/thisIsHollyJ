242054407924ae6489037569008cea385cae6ef7
holjas <holly.jasiura@gmail.com>
2021-06-11 16:49:32 -0400

    :zap:

--- js/colours.js
+++ js/colours.js
@@ -1,16 +1,13 @@
 const colourThemes = [
-    { colorOne: "30, 59, 52", colorTwo: "252, 108, 17" },
-    { colorOne: "255, 209, 0", colorTwo: "222, 16, 222" },
-    { colorOne: "245, 204, 207", colorTwo: "1, 178, 139" },
-    { colorOne: "252, 59, 45", colorTwo: "14, 47, 86" },
-    { colorOne: "17,35,120", colorTwo: "246, 74, 0" },
-    { colorOne: "40,41,43", colorTwo: "255,0,70" },
-    { colorOne: "229, 232, 220", colorTwo: "246, 116, 141" },
-
+  { colorOne: "30, 59, 52", colorTwo: "252, 108, 17" },
+  { colorOne: "255, 209, 0", colorTwo: "222, 16, 222" },
+  { colorOne: "245, 204, 207", colorTwo: "1, 178, 139" },
+  { colorOne: "252, 59, 45", colorTwo: "14, 47, 86" },
+  { colorOne: "17,35,120", colorTwo: "246, 74, 0" },
+  { colorOne: "40,41,43", colorTwo: "255,0,70" },
+  { colorOne: "229, 232, 220", colorTwo: "246, 116, 141" },
 ];
-
-
-
+console.log("connected");
 // Green 30, 59, 52
 // Orange 252, 108, 17
 
