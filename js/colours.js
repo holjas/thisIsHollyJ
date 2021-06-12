5a2425ccbb334a3938ae4e54fa5d7b779d1a6cac
holjas <holly.jasiura@gmail.com>
2021-06-12 19:41:49 -0400

    working on color profiles

--- js/colours.js
+++ js/colours.js
@@ -1,13 +1,23 @@
-// const colourThemes = [
-//   { colorOne: "30, 59, 52", colorTwo: "252, 108, 17" },
-//   { colorOne: "255, 209, 0", colorTwo: "222, 16, 222" },
-//   { colorOne: "245, 204, 207", colorTwo: "1, 178, 139" },
-//   { colorOne: "252, 59, 45", colorTwo: "14, 47, 86" },
-//   { colorOne: "17,35,120", colorTwo: "246, 74, 0" },
-//   { colorOne: "40,41,43", colorTwo: "255,0,70" },
-//   { colorOne: "229, 232, 220", colorTwo: "246, 116, 141" },
-// ];
-console.log("COLOURS IS CONNETED");
+const colourThemes = [
+  { colorOne: "30, 59, 52", colorTwo: "252, 108, 17" },
+  { colorOne: "255, 209, 0", colorTwo: "222, 16, 222" },
+  { colorOne: "245, 204, 207", colorTwo: "1, 178, 139" },
+  { colorOne: "252, 59, 45", colorTwo: "14, 47, 86" },
+  { colorOne: "17,35,120", colorTwo: "246, 74, 0" },
+  { colorOne: "40,41,43", colorTwo: "255,0,70" },
+  { colorOne: "229, 232, 220", colorTwo: "246, 116, 141" },
+  { colorOne: "55,23,34", colorTwo: "187,171,155" },
+  { colorOne: "22,155,163", colorTwo: "255,253,231" },
+  { colorOne: "102,20,200", colorTwo: "242,90,16" },
+  { colorOne: "1,143,211", colorTwo: "244,199,238" },
+  { colorOne: "30,59,52", colorTwo: "254,188,0" },
+  { colorOne: "40,41,43", colorTwo: "177,253,234" },
+  { colorOne: "255,140,139", colorTwo: "30,59,52" },
+  { colorOne: "255,43,147", colorTwo: "124,244,230" },
+  { colorOne: "187,171,155", colorTwo: "55,23,34" },
+  { colorOne: "40,41,43", colorTwo: "238,217,116" },
+  { colorOne: "198,215,199", colorTwo: "255,80,52" },
+];
 
 const myWebsite = {};
 
@@ -17,18 +27,33 @@ myWebsite.homepageAnimation = () => {
     // animation types
     // http://daneden.github.io/animate.css/
     animation: "fadeIn",
-    // animation: "fadeInUp",
-
-    // custom sprator
     seprator: ",",
-
-    // animation speed in ms
     speed: "3000",
   });
 };
 
+myWebsite.colourChanges = () => {
+  $("#colourChanger").click(function (e) {
+    e.preventDefault();
+
+    const randomNumber = myWebsite.generateRandomNumber();
+    const backgroundColour = "rgb(" + colourThemes[randomNumber].colorOne + ")";
+    const fontColour = "rgb(" + colourThemes[randomNumber].colorTwo + ")";
+
+    $("body").css("background-color", backgroundColour);
+    $("body").css("color", fontColour);
+    $("li a").css("color", fontColour);
+    $(".fa-exchange-alt").css("color", fontColour);
+  });
+};
+
+myWebsite.generateRandomNumber = () => {
+  return Math.floor(Math.random() * colourThemes.length);
+};
+
 myWebsite.init = () => {
   myWebsite.homepageAnimation();
+  myWebsite.colourChanges();
 };
 
 myWebsite.init();
